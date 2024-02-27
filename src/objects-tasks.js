9eeb936f9739bfe8b315f0a58bff344211cf1124
nivarnas <rpankratoff@gmail.com>
2024-02-27 18:16:28 +0300

    feat: add solution sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -150,8 +150,36 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const bills = {
+    25: 0,
+    50: 0,
+    100: 0,
+  };
+
+  return queue.every((bill) => {
+    if (bill === 25) {
+      bills[25] += 1;
+    } else if (bill === 50) {
+      if (bills[25] > 0) {
+        bills[25] -= 1;
+        bills[50] += 1;
+      } else {
+        return false;
+      }
+    } else if (bill === 100) {
+      if (bills[50] > 0 && bills[25] > 0) {
+        bills[25] -= 1;
+        bills[50] -= 1;
+        bills[100] += 1;
+      } else if (bills[25] > 2) {
+        bills[25] -= 3;
+        bills[100] += 1;
+      } else return false;
+    }
+
+    return true;
+  });
 }
 
 /**
